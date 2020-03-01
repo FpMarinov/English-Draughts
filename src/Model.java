fea26f1ef100f299286ffb9cd1b6ede8a5fedcc6
Filip <2175499M@student.gla.ac.uk>
2020-03-01 22:20:28 +0000

    Game over issue(doesnt affect draw proposal): on victory, both players get game over, only one gets new game about to begin, then the game freezes

--- src/Model.java
+++ src/Model.java
@@ -96,13 +96,18 @@ public class Model {
      */
     public boolean proposeActivePieceMove(int newRow, int newColumn) throws IllegalMoveException {
         if(board.isLegalMove(activePiece,newRow,newColumn)) {
+            //(legal move) and (can jump) = move is a jump
+            boolean isMoveAJump = board.pieceCanJump(activePiece);
+
             boolean becameKing = board.movePiece(activePiece,newRow,newColumn);
 
             if(becameKing) {
                 return false;
             }
 
-            return board.pieceCanJump(activePiece);
+            //if the previous move was a jump and the piece
+            //can still jump it must
+            return isMoveAJump && board.pieceCanJump(activePiece);
         } else {
             throw new IllegalMoveException();
         }
